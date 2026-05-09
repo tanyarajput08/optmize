e69cb811cc503b71ce12ffacf1be9a436bbb11c7
tanyarajput08 <tanyarajput121203@gmail.com>
2026-05-09 12:38:43 +0530

    Production API URL added

--- optmize/client/src/services/api.js
+++ optmize/client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL:
+    "https://optmize-production.up.railway.app/api"
 });
 
 export default api;
